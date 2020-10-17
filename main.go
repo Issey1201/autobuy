8224af994847344f01ec565ec33b8cc8676a0f25
Issey Takai <takai.1201@icloud.com>
2020-10-17 10:04:29 +0900

    [mod] 複数のURL対応

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"sync"
 
 	"github.com/Issey1201/pkg/autobuy"
 	"github.com/Issey1201/pkg/notify"
@@ -17,14 +16,11 @@ func main() {
 	// go run main.go ark amazon newEgg
 	ark := autobuy.NewArk(fmt.Sprintf("./config/%v.toml", targets[0]))
 
-	wg := new(sync.WaitGroup)
 	ch := make(chan autobuy.CheckResponse, len(ark.Config.Url.TargetUrl))
 	var err error
 
 	for _, v := range ark.Config.Url.TargetUrl {
-		// せっかくのgoなのでtargetUrlの数だけgoroutine回す
-		wg.Add(1)
-		go autobuy.CheckStock(ark, v, ch, wg)
+		go autobuy.CheckStock(ark, v, ch)
 	}
 
 	for v := range ch {
@@ -37,30 +33,10 @@ func main() {
 			if err = notify.Notificator(); err != nil {
 				fmt.Println("Failed to Notificator")
 			}
+			close(ch)
 			break
 		case false:
 			fmt.Println(v.Url, ": 在庫なし")
 		}
 	}
-	//wg.Wait()
-	fmt.Println("ぬけたた")
-	//for {
-	//	select {
-	//	case res := <- ch:
-	//		switch res.StockStatus {
-	//		case true:
-	//			wg.Wait()
-	//			fmt.Println(res.Url, ": 在庫ある")
-	//			if err = ark.Run(res.Url); err != nil {
-	//				log.Fatalln("Failed to run")
-	//			}
-	//			if err = notify.Notificator(); err != nil {
-	//				fmt.Println("Failed to Notificator")
-	//			}
-	//			os.Exit(1)
-	//		case false:
-	//			fmt.Println(res.Url, ": 在庫なし")
-	//		}
-	//	}
-	//}
 }
